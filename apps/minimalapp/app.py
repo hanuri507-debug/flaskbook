8bba9c49c5f436b0d14daaf03fafaa0a3d0df386
sehui park <hanuri507@gmail.com>
2026-02-07 22:32:11 +0900

    오늘 작성한 코드

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
+from email_validator import validate_email, EmailNotValidError
+from flask import (Flask, render_template, url_for, redirect, current_app, g, request, flash, )
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "ehfiwued273AHbheJ"
 
 @app.route("/")
 def index():
@@ -10,16 +12,61 @@ def index():
             methods=["GET", "POST"],
             endpoint="hello-endpoint")
 def hello(name):
-    return "Hello, {name}!"
+    return "Hello, {}!".format(name)
 
 @app.route("/name/<name>")
 def show_name(name):
     return render_template("index.html", name=name)
 
 with app.test_request_context():
-    # /
+    # /dz
     print(url_for("index"))
     # /hello/world
     print(url_for("hello-endpoint", name="world"))
     # /name/AK?page=1
     print(url_for("show_name", name="AK", page="1"))
+
+    with app.test_request_context("/users?updated=true"):
+        print(request.args.get("updated"))
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/contact/complete", methods=["GET", "POST"])
+def contact_complete():
+    if request.method == "POST":
+        # 폼 속성 사용해서 폼의 값을 취득
+        username = request.form.get("username","").strip()
+        email = request.form.get("email","").strip()
+        description = request.form.get("description","").strip()
+
+        is_valid = True
+
+        if not username:
+            flash("사용자명을 입력해 주세요")
+            is_valid = False
+
+        if not email:
+            flash("메일 주소를 입력해 주세요")
+            is_valid = False
+
+        else:
+            try:
+                validate_email(email)
+            except EmailNotValidError:
+                flash("형식에 맞는 메일 주소를 입력해 주세요")
+                is_valid = False
+
+        if not description:
+            flash("문의 내용을 입력해 주세요")
+            is_valid = False
+
+        if not is_valid:
+            return redirect(url_for("contact"))
+        # 이메일 보내기
+
+        flash("문의해 주셔서 감사합니다(✿◡‿◡)")
+        return redirect(url_for("contact_complete"))
+    
+    return render_template("contact_complete.html")
